9511b083162a4bec856a2fbaebdeea6e58dda0b8
isaac <isaac.razabi@gmail.com>
2021-07-14 14:41:37 +0300

    some more work

---
+++ src/components/stay-filter.vue
@@ -0,0 +1,89 @@
+<template>
+  <form @submit.prevent="setFilter">
+    <section class="stay-filter">
+      <div class="filter-wrraper">
+        <span class="location">
+          <label> location </label>
+          <input
+            @input="setFilter"
+            v-model="filterBy.loc"
+            type="text"
+            placeholder="where are you going? 🔍"
+          />
+        </span>
+        <span class="cheak-in">
+          <label for="cheak in">cheak in</label>
+          <input v-model="filterBy.dateStart" type="date" />
+        </span>
+        <span class="cheak-out">
+          <label for="cheak out"> cheak out </label>
+          <input v-model="filterBy.dateEnd" type="date" />
+        </span>
+        <span class="guests">
+          <label for="guests"> add guests </label>
+          <input type="number" v-model="filterBy.guests" />
+        </span>
+        <button>🔎</button>
+      </div>
+    </section>
+  </form>
+</template>
+
+
+<script>
+export default {
+  name: "stay-filter",
+  components: {},
+  props: {},
+  created() {},
+  data() {
+    return {
+      filterBy: {
+        loc: "",
+        dateStart: new Date().toISOString().substr(0, 10),
+        dateEnd: new Date().toISOString().substr(0, 10),
+        guests: 0,
+      },
+      isSowen: false,
+    };
+  },
+  computed: {},
+  methods: {
+    setFilter() {
+      this.$emit("filter", JSON.parse(JSON.stringify(this.filterBy)));
+    },
+    // displayInput(){
+    //     this.isSowen = !isSowen
+    // }
+  },
+};
+</script>
+
+<style scoped>
+.location {
+  display: flex;
+  flex-direction: column;
+}
+/* .location::after{
+  content: 'location';
+} */
+.cheak-in {
+  display: flex;
+  flex-direction: column;
+}
+.cheak-out {
+  display: flex;
+  flex-direction: column;
+}
+.guests {
+  display: flex;
+  flex-direction: column;
+}
+.filter-wrraper {
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  margin-inline-start: 8px;
+  margin-inline-end: 8px;
+}
+</style>
---
+++ src/components/stay-signup.vue
@@ -0,0 +1,131 @@
+    <template>
+      <section>
+    <form  @submit.prevent="login">
+      <label for="">login</label>
+      <input
+        type="text"
+        placeholder="username"
+        v-model="credencials.username"
+      />
+      <input
+        type="password"
+        placeholder="password"
+        v-model="credencials.password"
+      />
+    </form>
+    <div class="sign-up" v-if="!loggedInUser">
+            <label for="">sign up</label>
+      <form @submit.prevent="signup">
+        <input
+          type="text"
+          placeholder="username"
+          v-model="signUpCredencials.username"
+        />
+        <input
+          type="text"
+          placeholder="Your full name"
+          v-model="signUpCredencials.fullname"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          v-model="signUpCredencials.password"
+        />
+        <button>Signup</button>
+      </form>
+    </div>
+      </section>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return {
+      credencials: { username: "", password: "" },
+      signUpCredencials: {
+        username: "",
+        password: "",
+        fullname: "",
+        isAdmin: false,
+      },
+      loggedInUser: this.$store.getters.loggedinUser,
+    };
+  },
+  methods: {
+
+        login() {
+      this.$store
+        .dispatch({ type: "login", userCred: this.credencials })
+        .then((user) => {
+          this.loggedInUser = user;
+          this.credencials = { username: "", password: "" };
+          this.$router.push("/toy-app");
+        });
+    },
+    signup() {
+      this.$store
+        .dispatch({ type: "signup", userCred: this.signUpCredencials })
+        .then((user) => {
+          this.loggedInUser = user;
+          // this.submitTo();
+          this.signUpCredencials = {
+            username: "",
+            password: "",
+            fullname: "",
+            isAdmin: false,
+          };
+          this.$router.push("/home");
+        });
+    },
+    logout() {
+      this.$store
+        .dispatch({ type: "logout" })
+        .then(() => (this.loggedInUser = null));
+    },
+      //   createAutComplete(names){
+      // this.completeWords = names
+    // },
+    //   submitTo() {
+    //   let user = this.credencials.username.substring(0,this.credencials.username.indexOf("@"));
+    //   if ((this.searchWords).includes(user)) return; 
+    //   this.searchWords.push(user);
+    //   mailService.saveEmailNames(this.searchWords)
+    // },
+  },
+  components: {
+
+  },
+    // watch: {
+    // credencials: {
+    //   handler(newVal, oldVal) {
+    //     for (let index = 0; index < this.searchWords.length; index++) {
+    //       if (newVal.to[0] === this.searchWords[index][0]) {
+    //         const filteredNames = this.searchWords.filter((name)=>{
+    //           if (name[0]===newVal.to[0]) return name
+    //         })
+    //         this.createAutComplete(filteredNames)
+    //       }
+    //     }
+        
+    //   },
+    //   deep: true,
+    // },
+    
+  // },
+
+};
+</script>
+
+<style scoped>
+
+
+</style>
+
+
+
+
+
+
+
