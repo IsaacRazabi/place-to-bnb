858ab8998975cd58d061fc9f4a73cbd8a7039f63
AryeAskarov <arye.ask@gmail.com>
2021-07-20 11:02:07 +0300

    11:00 start

---
+++ src/components/superhost.vue
@@ -0,0 +1,16 @@
+<template>
+    <section>
+        <navBar class="navBar"></navBar>
+        <img src="../assets/examp/Airbnb-Superhost-4.jpg">
+        <h2>hello</h2>
+    </section>
+</template>
+
+<script>
+import navBar from "../components/nav-bar.vue";
+export default {
+    components: {
+    navBar,
+  },
+}
+</script>
